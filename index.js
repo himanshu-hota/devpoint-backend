7d071b67c95dcce3c1048e16bb99a62a4c3cd659
Himanshu Hota <Himanshuhota55@gmail.com>
2023-12-20 22:37:27 +0530

    cors fix 0.7

--- index.js
+++ index.js
@@ -30,11 +30,10 @@ app.use(cookieParser())
 app.use(cors());
 // Enable CORS for all routes
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://devpoint-frontend.vercel.app/');
+    res.header('Access-Control-Allow-Origin', 'https://devpoint-frontend.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-
+    
     next();
 });
 
